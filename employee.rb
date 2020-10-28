f28c6616badda7520fed917f96d9eab9d22d3a24
jimmy <jonssonjimmy8@gmail.com>
2020-10-27 21:09:11 -0400

    changing store item into class

---
+++ employee.rb
@@ -0,0 +1,11 @@
+employee_records = []
+
+puts "enter your name:"
+      employee_name = gets.chom p
+puts "enter your last name"
+employee_last_name = gets.chomp
+puts "whats your estimated salary"
+salary = gets.chomp.to_s
+
+record = []
+record << employee_name employee_last_name salary
---
+++ store_item.rb
@@ -0,0 +1,11 @@
+bike1 = {type: "mountain bike", model_name: "Santa Cruz", frame_material: "titanium"}
+
+p "I'm looking for a #{bike1[:model_name]} #{bike1[:type]} with a #{bike1[:frame_material]} frame."
+
+bike2 = {type: "mountain bike", model_name: "Salsa", frame_size: "186cm" }
+
+p "I'm looking for a #{bike2[:model_name]} #{bike2[:type]} with a #{bike2[:frame_size]} frame."
+
+bike3 = {type: "trail bike", model_name: "Yeti", frame_material: "carbon"}
+
+p "I'm looking for a #{bike3[:model_name]} #{bike3[:type]} with a #{bike3[:frame_material]} frame."
