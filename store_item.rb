6af3b709c55ded0726c573bff32cf59a0fea3631
jimmy <jonssonjimmy8@gmail.com>
2020-11-13 18:51:07 -0500

    whiteboard exercise, store item ruby exercise

--- store_item.rb
+++ store_item.rb
@@ -1,11 +1,49 @@
-bike1 = {type: "mountain bike", model_name: "Santa Cruz", frame_material: "titanium"}
+class Item
 
-p "I'm looking for a #{bike1[:model_name]} #{bike1[:type]} with a #{bike1[:frame_material]} frame."
+  attr_reader :type, :model_name, :frame_material, :frame_size
+  attr_writer :type, :model_name, :frame_material, :frame_size
 
-bike2 = {type: "mountain bike", model_name: "Salsa", frame_size: "186cm" }
+  def initialize(input_options)
+    @type = input_options[:type]
+    @model_name = input_options[:model_name]
+    @frame_material = input_options[:frame_material]
+    @frame_size = input_options[:frame_size]
+  end
 
-p "I'm looking for a #{bike2[:model_name]} #{bike2[:type]} with a #{bike2[:frame_size]} frame."
+  def type
+    @type   
+  end
 
-bike3 = {type: "trail bike", model_name: "Yeti", frame_material: "carbon"}
+  def model_name
+    @model_name
+  end
 
-p "I'm looking for a #{bike3[:model_name]} #{bike3[:type]} with a #{bike3[:frame_material]} frame."
+  def frame_material
+    @frame_material
+  end
+
+  def frame_size
+    @frame_size
+  end
+  def print_info
+    p "Hi, do you have #{@model_name} #{@type} with a #{@frame_material}?!"
+  end
+
+  p "starting"
+end
+
+bike1 = Item.new(type:, model_name:)
+
+p bike1.print_info
+
+# bike1 = {type: "mountain bike", model_name: "Santa Cruz", frame_material: "titanium"}
+
+# p "I'm looking for a #{bike1[:model_name]} #{bike1[:type]} with a #{bike1[:frame_material]} frame."
+
+# bike2 = {type: "mountain bike", model_name: "Salsa", frame_size: "186cm" }
+
+# p "I'm looking for a #{bike2[:model_name]} #{bike2[:type]} with a #{bike2[:frame_size]} frame."
+
+# bike3 = {type: "trail bike", model_name: "Yeti", frame_material: "carbon"}
+
+# p "I'm looking for a #{bike3[:model_name]} #{bike3[:type]} with a #{bike3[:frame_material]} frame."
---
+++ whiteboard_exe.rb
@@ -0,0 +1,9 @@
+numbers_rays = [8, 5 ,10, 15]
+i = 0
+reversed_num = []
+while i < numbers_rays.length
+  reversed_num << numbers_rays[i]
+  i += 1
+end
+
+p reversed_num.reverse
